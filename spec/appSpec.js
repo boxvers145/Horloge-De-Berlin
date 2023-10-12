321cc5680b645bc2af358ceef8d7ab4a80d231db
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-10-12 18:10:37 +0200

    refrector : describe and minuteLine to work great

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,50 +1,60 @@
 import { BerlinClock } from '../src/app.js';
 
-describe("the first minute line", function(){
+describe("minuteline", function(){
     //arrange
     let clock = new BerlinClock();
 
-    it("MinuteLine given 0 should return 0", function() {
+    it("given 0 should return 0", function() {
         let result = clock.minuteLine(0);
         expect(result).toBe("0000")
     })
 
-    it("MinuteLine given 1 should return 1", function() {
+    it("given 1 should return 1", function() {
         let result = clock.minuteLine(1);
         expect(result).toBe("I000")
     })
 
-    it("MinuteLine given 2 should return 2", function() {
+    it("given 2 should return 2", function() {
         let result = clock.minuteLine(2);
         expect(result).toBe("II00")
     })
 
-    it("MinuteLine given 3 should return 3", function() {
+    it("given 3 should return 3", function() {
         let result = clock.minuteLine(3);
         expect(result).toBe("III0")
     })
 
-    it("MinuteLine given 4 should return 4", function() {
+    it("given 4 should return 4", function() {
         let result = clock.minuteLine(4);
         expect(result).toBe("IIII")
     })
 
-    it("MinuteLine given 5 should return 5", function() {
+    it("given 5 should return 5", function() {
         let result = clock.minuteLine(5);
         expect(result).toBe("0000")
     })
 
-    it("MinuteLine given 7 should return II00", function() {
+    it("given 6 should return I000", function() {
+        let result = clock.minuteLine(6);
+        expect(result).toBe("I000")
+    })
+
+    it("given 7 should return II00", function() {
         let result = clock.minuteLine(7);
         expect(result).toBe("II00")
     })
 
-    it("MinuteLine given 9 should return III0", function() {
+    it("given 8 should return III0", function() {
+        let result = clock.minuteLine(8);
+        expect(result).toBe("III0")
+    })
+
+    it("given 9 should return III0", function() {
         let result = clock.minuteLine(9);
         expect(result).toBe("IIII")
     })
 
-    it("MinuteLine given 10 should return 0000", function() {
+    it("given 10 should return 0000", function() {
         let result = clock.minuteLine(10);
         expect(result).toBe("0000")
     })
--- src/app.js
+++ src/app.js
@@ -1,12 +1,52 @@
 
 export class BerlinClock{
     minuteLine(time){
-        if (time == 0 || time%5 == 0) { return "0000"; }
-        if (time == 1 || time%6 == 0) { return "I000"; }
-        if (time == 2 || time%7 == 0) { return "II00"; }
-        if (time == 3 || time%8 == 0) { return "III0"; }
-        if (time == 4 || time%9 == 0) { return "IIII"; }
+        if(this.isZeroOrMultipleOf5(time)){return "0000"}
+        if (this.isOneOrMultipleOf6(time)) { return "I000"; }
+        if (this.isTwooOrMultipleOf7(time)) { return "II00"; }
+        if (this.isThreeOrMultipleOf8(time)) { return "III0"; }
+        if (this.isFourOrMultipleOf9(time)) { return "IIII"; }
+s
+        return null;
+    }
 
-        return "0000";
+    isZeroOrMultipleOf5(time){
+        if(time == 0 || time%5 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    isOneOrMultipleOf6(time){
+        if(time == 1 || time%6 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    isTwooOrMultipleOf7(time){
+        if(time == 2 || time%7 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    isThreeOrMultipleOf8(time){
+        if(time == 3 || time%8 == 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    isFourOrMultipleOf9(time){
+        if(time == 4 || time%9 == 0){
+            return true;
+        }
+
+        return false;
     }
 }
