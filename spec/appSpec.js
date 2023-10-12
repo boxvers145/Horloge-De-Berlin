ffe1809d08bbdc2c8d61f007eaea0582b7553a8c
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-10-12 17:58:46 +0200

    add : test first line minutes

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,23 +1,51 @@
-import { Main } from '../src/app.js';
+import { BerlinClock } from '../src/app.js';
 
-describe("A suite of test for the demo function of the main class", function(){
-    let main = new Main();
+describe("the first minute line", function(){
+    //arrange
+    let clock = new BerlinClock();
 
-    it("fizzbuzz function should return 1 when given 1", function() {
-        let result = main.fizzByzz(1);
-
-        expect(result).toBe(1)
+    it("MinuteLine given 0 should return 0", function() {
+        let result = clock.minuteLine(0);
+        expect(result).toBe("0000")
     })
 
-    it("fizzbuzz function should return 2 when given 2", function() {
-        let result = main.fizzByzz(2);
-
-        expect(result).toBe(2)
+    it("MinuteLine given 1 should return 1", function() {
+        let result = clock.minuteLine(1);
+        expect(result).toBe("I000")
     })
 
-    it("fizzbuzz function should return fizz when given 3", function() {
-        let result = main.fizzByzz(3);
+    it("MinuteLine given 2 should return 2", function() {
+        let result = clock.minuteLine(2);
+        expect(result).toBe("II00")
+    })
 
-        expect(result).toBe("fizz")
+    it("MinuteLine given 3 should return 3", function() {
+        let result = clock.minuteLine(3);
+        expect(result).toBe("III0")
+    })
+
+    it("MinuteLine given 4 should return 4", function() {
+        let result = clock.minuteLine(4);
+        expect(result).toBe("IIII")
+    })
+
+    it("MinuteLine given 5 should return 5", function() {
+        let result = clock.minuteLine(5);
+        expect(result).toBe("0000")
+    })
+
+    it("MinuteLine given 7 should return II00", function() {
+        let result = clock.minuteLine(7);
+        expect(result).toBe("II00")
+    })
+
+    it("MinuteLine given 9 should return III0", function() {
+        let result = clock.minuteLine(9);
+        expect(result).toBe("IIII")
+    })
+
+    it("MinuteLine given 10 should return 0000", function() {
+        let result = clock.minuteLine(10);
+        expect(result).toBe("0000")
     })
 });
--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 
-export class Main{
-    fizzByzz(number) {
-        if (number == 3) { return "fizz"};
+export class BerlinClock{
+    minuteLine(time){
+        if (time == 0 || time%5 == 0) { return "0000"; }
+        if (time == 1 || time%6 == 0) { return "I000"; }
+        if (time == 2 || time%7 == 0) { return "II00"; }
+        if (time == 3 || time%8 == 0) { return "III0"; }
+        if (time == 4 || time%9 == 0) { return "IIII"; }
 
-        return number;
+        return "0000";
     }
 }
